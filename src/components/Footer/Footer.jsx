2eadd601f31bf64a1026951b62d307f8700591ed
RoboMurlok <121564225+RoboMurlok@users.noreply.github.com>
2025-12-04 10:08:51 +0200

    Add social media icons to Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,21 @@ export default function Footer() {
   return (
     <footer>
       <div className="up">
-        <div className="box">
+        <div className="cards icons">
+          <Link to="/service">
+            <img src="src/assets/logo-instagram.svg" alt="" />
+          </Link>
+          <Link to="/service">
+            <img src="src/assets/logo-twitter.svg" alt="" />
+          </Link>
+          <Link to="/service">
+            <img src="src/assets/logo-facebook.svg" alt="" />
+          </Link>
+        </div>
+        {/* <div className="box">
           <h4>Da el primer paso hacia una gestión financiera más ordenada.</h4>
           <Link to="/service" className="btn">Agendar una consulta</Link>
-        </div>
+        </div> */}
       </div>
       <div className="down">
         © 2025 artcas.beauty. Todos los derechos reservados.
